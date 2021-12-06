c9ef24fd088128b8d8cf8bbc197b89e489bf2961
Sung Yoo <yoowook1207@gmail.com>
2021-12-06 17:22:18 -0500

    dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
-
-var buttonEl = document.querySelector("#save-task");
+let formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
